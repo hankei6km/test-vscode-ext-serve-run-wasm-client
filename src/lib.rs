5ba029a7f7b0ef8c7084c6dfc2bcbccb66c0b0c6
hankei6km <gh-hankei6km@outlook.jp>
2023-10-20 10:43:29 +0000

    feat: Make "build_url"

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use run::RunArgs;
 use serde_json::de::IoRead;
 use serde_json::{StreamDeserializer, Value};
 use std::io::{BufReader, BufWriter, Read, Write};
@@ -59,11 +60,62 @@ mod tests {
     }
 }
 
+use serde::Serialize;
+use url::Url;
+#[derive(Serialize)]
+struct ArgsArgs {
+    args: Vec<String>,
+}
+
+fn build_url(run_args: RunArgs) -> Url {
+    let mut url = Url::parse("http://localhost/run").unwrap();
+
+    let mut args: Vec<String> = vec![
+        "--memory_initial".to_string(),
+        run_args.memory_initial.to_string(),
+        "--memory_maximum".to_string(),
+        run_args.memory_maximum.to_string(),
+        "--memory_shared".to_string(),
+        "--".to_string(),
+    ];
+    args.extend(run_args.files);
+    let args_json_array = serde_json::to_string(&args).unwrap();
+    url.query_pairs_mut()
+        .append_pair("args", &args_json_array)
+        .finish();
+
+    url
+}
+#[cfg(test)]
+mod test_build_url {
+    use super::*;
+
+    #[test]
+    fn test_build_url() {
+        let args = RunArgs {
+            memory_initial: 1,
+            memory_maximum: 2,
+            memory_shared: true,
+            files: vec![
+                "test1.wasm".to_string(),
+                "--foo".to_string(),
+                "bar".to_string(),
+            ],
+        };
+        let url = build_url(args);
+        assert_eq!(
+            url.as_str(),
+            "http://localhost/run?args=%5B%22--memory_initial%22%2C%221%22%2C%22--memory_maximum%22%2C%222%22%2C%22--memory_shared%22%2C%22--%22%2C%22test1.wasm%22%2C%22--foo%22%2C%22bar%22%5D"
+        );
+    }
+}
+
 pub mod run {
     use serde_json::{Deserializer, Value};
     use std::io::{BufReader, BufWriter};
+    use url::Url;
 
-    use crate::output;
+    use crate::{build_url, output};
 
     pub struct RunArgs {
         pub memory_initial: u32,
@@ -72,19 +124,13 @@ pub mod run {
         pub files: Vec<String>,
     }
     pub struct Run {
-        //memory_initial: u32,
-        //memory_maximum: u32,
-        //memory_shared: bool,
-        //files: Vec<String>,
+        url: Url,
     }
 
     impl Run {
-        pub fn new(_args: RunArgs) -> Self {
+        pub fn new(run_args: RunArgs) -> Self {
             Self {
-                //memory_initial: args.memory_initial,
-                //memory_maximum: args.memory_maximum,
-                //memory_shared: args.memory_shared,
-                //files: args.files,
+                url: build_url(run_args),
             }
         }
         pub fn run(&self) {
@@ -103,6 +149,7 @@ pub mod run {
                 BufWriter::new(std::io::stdout()),
                 BufWriter::new(std::io::stderr()),
             );
+            println!("{}", self.url);
             //for value in stream {
             //    println!("-{}", value.unwrap());
             //}
